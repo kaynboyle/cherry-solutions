a5d026828ea939b6a48844ce0871ad7aba29f0f6
Kaylin Boyle <kaylinnboyle@gmail.com>
2022-11-10 18:42:45 -0800

    header and home have index and css

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+const Header = () => {
+
+    return (
+        <main>
+        </main>
+ 
+ ); 
+};
+
+export default Header;
