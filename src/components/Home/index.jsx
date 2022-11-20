355a83556952d44c1d5b3570e5270927efddbdcc
Kaylin Boyle <90358012+kaynboyle@users.noreply.github.com>
2022-11-19 19:46:23 -0800

    home edited as well (#6)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,7 +2,28 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const Home = () => {
-  return <main></main>;
+  return(
+    <main>
+      <div className="shadow-2xl">
+        <div className="hero-image">
+          <div className="shadow-2xl" id="hero-text">
+            <div className="background">
+              <p>Optimizing Container Terminal  Performace, 
+                Tredictability & Transparancy.
+              </p>
+              <button id="home-header-button" className="button is-info">Learn More</button>
+              <p> </p>
+              <p>Integrating People, Process & Technology 
+                by applying professional and solution serivices
+                for configuring and installing 'Success by Design'
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+  );
 };
 
 export default Home;
