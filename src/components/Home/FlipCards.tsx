57c7133843d53a713176d191d6e4651234055e79
Kaylin Boyle <kaylinnboyle@gmail.com>
2022-11-23 23:03:25 -0800

    flipping boxes done

--- src/components/Home/FlipCards.tsx
+++ src/components/Home/FlipCards.tsx
@@ -1,13 +1,17 @@
 
 // {firstName, middleName = "N/A", lastName}:OptionalMiddleName)
+import cn from "classnames";
+import React, {Component} from 'react';
 
 
-interface cards{
-    id: string,
-    variant: string,
-    front: string,
-    back: string
-;}
+interface cards
+  {
+  id: string,
+  variant: string,
+  front: string,
+  back: string,
+};
+
 
 function FlipCard(props:cards) {
     return (
