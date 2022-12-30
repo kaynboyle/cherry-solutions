802fecc58c9c64de65ed2ff719047d3e7aecb7b5
Kaylin Boyle <90358012+kaynboyle@users.noreply.github.com>
2022-12-29 22:39:54 -0800

    home and consulting (#12)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,19 +22,19 @@ const Header = () => {
           <Navbar.Toggle className="here" aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <Nav.Link className="link-color"  href="./consulting">Consulting</Nav.Link>
+              <Nav.Link className="link-color" href="#pricing">Pricing</Nav.Link>
+              <NavDropdown className="link-color" title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link className="here" href="#deets">
+              <Nav.Link className="link-color" href="#deets">
                 More deets
               </Nav.Link>
-              <Nav.Link eventKey={2} href="./contact-us">
+              <Nav.Link className="link-color" eventKey={2} href="./contact-us">
                 Contact Us
               </Nav.Link>
             </Nav>
