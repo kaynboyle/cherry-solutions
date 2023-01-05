08d121e5f0163a7df505b77d8715f84c69647494
Kaylin Boyle <90358012+kaynboyle@users.noreply.github.com>
2023-01-05 00:00:21 -0800

    please save (#16)

--- src/components/Consulting/index.tsx
+++ src/components/Consulting/index.tsx
@@ -25,10 +25,10 @@ const Consulting = () => {
                 <h1>Consulting</h1>
                 <div className="consultingHeader">
                     <div>
-                        <h2>Performance Intelligence</h2>
+                        <p className="button-slide slide-right">Performance Intelligence</p>
                     </div>
                     <div>
-                        <h2>Applied Intelligence</h2>
+                        <p className="button-slide slide-right">Applied Intelligence</p>
                     </div>
                     
                 </div>
@@ -45,21 +45,16 @@ const Consulting = () => {
                 <p className="left-text">Unlock profitable, sustainable growth based on a data-driven approach to optimizing operations.
                 To succeed today, you need to <strong className="strongANDred reveal"> engineer smarter</strong>,<strong className="strongANDred reveal"> <br></br>operate better</strong>, and <strong className="strongANDred reveal">drive sustainable efficiency</strong>.</p>
             </div>
-                <div className="arrow-flex">
-                    <div className="arrowIMGdiv reveal">
-                        <img src={require("../../assets/images/arrow.png")} alt="arrow"/>
-                    </div>
-                    <div className="box1row two">
-                            <p className="left-text">To do that you need more than data. <br></br>You need information and analytics working together so that<br></br> you
+                <div>
+                    <div className="box1row2">
+                            <p className="left-text">To do that...<br></br> You need more than data. <br></br>You need information and analytics working together so that<br></br> you
                                 can benefit from intelligence that scales.
                             </p>
                     </div>   
                 </div>
             <div className='consulting1-flex'>
                     
-                <div className="consultingCol1">
-                </div>
-                <div className="consultingCol1">
+                <div className="consultingCol1 reveal">
                     <div className="box1">
                             <p>
                                 Performance Intelligence <br></br>connects the power of information and AI to human insight giving the people
@@ -67,18 +62,72 @@ const Consulting = () => {
                                 
                             </p>
                      </div>
-                     <div className="box1">
+                     <div className="box1 ">
                         <p>Recognize
                                 new opportunities, accelerate digital transformation, realize growth, and drive sustainability.
                         </p>
                      </div>
-                     <div className="Consulting2img">
+                </div>
+                <div className="imageFlexcol">
                         <img src={require("../../assets/images/Consulting2.jpeg")} alt="consulting"/>
-                    </div>
-
                 </div>
                 
             </div>
+            <div className="box1row2 upper-filter">
+                <div className="logo-content-wrapper">
+                    <div>
+                        <p>Elevate your systems and empower your teams. With our processes, toolsets and metrics, Cherry America can help you get your processes under control and achieve ongoing peace of mind from:
+                        </p>
+                    </div>
+                    
+                        <ul>
+                            <li>Strategic Marine Terminal Operational Excellence 
+                            </li>
+                            <li>Data Analytics as a Service (DAaaS) 
+                            </li>
+                            <li>Tactical Sustainment & Governance 
+                            </li>
+                        </ul>
+                        
+                    </div>
+            </div>
+            <div className='header-wrap2'>
+
+            </div>
+            <div className="Consulting1img">
+                <div>
+                    <img className='consultingimg1' src={require("../../assets/images/Consulting1.jpeg")} alt="consulting"/>
+                    <div className="imageOverlay">
+                        <p >Applied Intelligence</p> 
+                    </div> 
+                </div>    
+            </div>
+            <div className="box2row">
+                <p className="left-text">Unlock profitable, sustainable growth based on a data-driven approach to optimizing operations.
+                To succeed today, you need to <strong className="strongANDred reveal"> engineer smarter</strong>,<strong className="strongANDred reveal"> <br></br>operate better</strong>, and <strong className="strongANDred reveal">drive sustainable efficiency</strong>.</p>
+            </div>
+
+            {/* <div className="consulting-logo">
+                <div className="logo-content-wrapper">
+                    <div>
+                        <p>Elevate your systems and empower your teams. With our processes, toolsets and metrics, Cherry America can help you get your processes under control and achieve ongoing peace of mind from:
+                        </p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                            Strategic Marine Terminal Operational Excellence 
+                            </li>
+                            <li>
+                            With Data Analytics as a Service (DAaaS) 
+                            </li>
+                            <li>
+                            Tactical Sustainment & Governance 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div> */}
             
                 
                
