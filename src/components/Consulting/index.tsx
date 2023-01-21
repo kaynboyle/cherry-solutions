e03681c3e1004f8a5e0f29bae6447d7117a428ec
Kaylin Boyle <90358012+kaynboyle@users.noreply.github.com>
2023-01-20 23:10:03 -0800

    consulting mos portion done (#28)

--- src/components/Consulting/index.tsx
+++ src/components/Consulting/index.tsx
@@ -73,20 +73,100 @@ const Consulting = () => {
                     <img id="performance-intelligence"className='consultingimg1 mobile-friend' src={require("../../assets/images/Consulting1.jpeg")} alt="consulting"/>
                 </div> 
                 <div className="imageOverlay">
-                        <p >What can Performance Intelligence do for you?</p> 
+                        <p >Management Operating System (MOS)</p> 
                 </div>    
             </div>
             <div className="box1row">
-                <p  className="left-text">Unlock profitable, sustainable growth based on a data-driven approach to optimizing operations.
-                To succeed today, you need to <strong className="strongANDred reveal"> engineer smarter</strong>,<strong className="strongANDred reveal"> <br></br>operate better</strong>, and <strong className="strongANDred reveal">drive sustainable efficiency</strong>.</p>
+                <p  className="left-text">A MOS is the set of tools, meetings and behaviors used to manage your people, processes, and technology to<strong className="strongANDred reveal"> deliver results</strong></p>
             </div>
-                <div>
-                    <div className="box1row2">
-                            <p className="left-text">To do that...<br></br> You need more than data. <br></br>You need information and analytics working together so that<br></br> you
-                                can benefit from intelligence that scales.
+              
+                <div id="mos" className="consultingIMG3">
+                <div className="box1row2 closer">
+                            <p className="left-text"> A Key Distinction TOS vs. MOS
                             </p>
                     </div>   
+                <div className="mos-title reveal">
+                    {/* <div className="mos-name"><p>Management Operating System<br></br>(MOS)</p></div> */}
+                    <div className="threeheight">
+                        <div ><p><strong><u>T</u>erminal <u>O</u>perating <u>S</u>ystem: A TOS</strong></p>
+                            <p>The <strong className="strong">TOS</strong> is a key part of a <strong>terminal</strong>, one primarily focused on the control, movement, and storage of various types of cargo in and around<br></br> a container <strong>terminal</strong>.</p><p>The <strong>TOS</strong> is part of the <strong>MOS</strong> controlling data within its expert functions and linking to an integrated management <br></br>tools and processes.</p>
+                        </div>
+                    </div>
+                    <div className="threeheight">
+                        <div >
+                            <p><strong><u>M</u>anagement <u>O</u>perating <u>S</u>ystem: A MOS</strong></p>
+                            <p>A <strong>MOS</strong> follows the Plan, Do, Check, Act improvement cycle to get control and steadily improve process performance. </p>
+                            <p>The <strong>MOS</strong> incorporates and employs a variety of expert systems <br></br>inclusive of ‘any’ <strong>TOS</strong>.</p>
+                            <p>The <strong>MOS</strong> creates ability to align, and performance measure the effective utilization and delivery of expected ROI of the investment across expert systems.</p>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div className="box1row">
+                <p  className="left-text">The MOS enables a strict process of <strong className="strongANDred reveal">Plan. Do. Check. Act.</strong> This improvement cycle enables <br></br>control and steady process performance improvement. </p>
+            </div>
+            <div className="proven-results">
+                    <p className="middle-p x-large"><strong>All Industrial Plants (Terminals) have a MOS</strong></p>
+                </div>
+            <div className="does-you-mos-work">
+                <div className="imageFlexcol consultingCol1 mos-circle">
+                    <div><img  src={require("../../assets/images/mos-circle-img.jpg")} alt="consulting"/></div>
+                    <div><p>Key Question:<br></br> ‘Does your MOS work?’</p></div> 
+                            
+
+                </div>
+            </div>
+            
+            <div>
+                <p className="mos-graph-title">Four Stages of MOS Development</p>
+                <div className="flex-graph">
+                    <div className="size-contain">
+                        <div className="graph-div">
+                                <div className="mos-height"></div>
+                        </div>
+                        <p>Pace of Improvment</p>
+                    </div>
+                    <div className="fightingfires-ect">
+                        <ul className="greater-ul">
+                            <li className="title-li"><strong> 1. Fighting Fires</strong><br></br>
+                                <ul>
+                                    <li>Working hard but losing ground</li>
+                                    <li>Lack of a coherent structure in operations management</li> 
+                                    <li>Addressing symptoms instead of root causes</li>
+                                    <li>Explaining away poor perfomance as "out of our control"</li>
+                                </ul>
+                            </li>
+                            <li className="title-li"><strong >2. Treading Water</strong>
+                                <ul>
+                                    <li>Solving problems at the same rate they are occuring</li>
+                                    <li>Meaningful but informal structure in place</li> 
+                                    <li>holding ground but not improving</li>
+                                    <li>High risk of derailment by a big event</li>
+                                </ul>
+                            </li>
+                            <li className="title-li"><strong>3. Incrementally Improving</strong>
+                                <ul>
+                                    <li>Steady incremental gains</li>
+                                    <li>Rigorous formal structure in place</li> 
+                                    <li>CI literate workforce; broad engagement of hearts and minds</li>
+                                    <li>Stringing towards world class; the best of continuous improvment</li>
+                                    <li>Risk of complacency</li>
+                                </ul>
+                            </li>
+                            <li className="title-li"><strong>4. Step Change Breakthroughs</strong>
+                                <ul>
+                                    <li>Achieving regular breakthrough improvements</li>
+                                    <li>Routine challenging of the conventional limits; going beyond benchmarking</li> 
+                                    <li>Strong, visible individual ownership for overall business performance</li>
+                                    <li>Redefining world class</li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
             <div className='consulting1-flex'>
                     
                 <div className="consultingCol1 reveal">
