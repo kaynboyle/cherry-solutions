af317b59a72da992bfc93f7ae4023944702e3e72
Kaylin Boyle <90358012+kaynboyle@users.noreply.github.com>
2023-03-08 22:17:43 -0600

    done (#34)

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -7,72 +7,78 @@ const AboutUs:FC = () => {
     window.addEventListener("scroll", reveal);
     return(
         <div className="App">
-            <div className="div1About">
-                <p>Cherry America provides professional consulting services and software solutions to the marine transportation industry.<br></br> Cherry America’s experience and tool sets lead optimization and change in all sizes of organizations.</p>
+            <div className="headerAbout">
+                <div className="width20per bgRED fXLARGE borders-20 fWEIGHT700 flex-center-image"><img className="cherryamericaIMG margin-top-20px"src={require("../../assets/images/whitecherry.png")} alt="consulting"/></div>
+                <div className="width80per text-align-center borders-20 fLARGE fWEIGHT700 margin-top-20px"><p>Cherry America provides professional consulting services and software solutions to the marine transportation industry. Cherry America’s experience and tool sets lead optimization and change in all sizes of organizations.</p></div>
             </div>
-            <div className="ribbon">
-                <strong className="ribbon-content">Cherry America brings 80+ years of combined experience working in marine terminal operations and the shipping industry.</strong>
-            </div>
-            <div className="div1About flex-about">
-                <div>
-                    <p>Cherry America provides professional consulting services and software solutions to the marine transportation industry.  Cherry America’s experience and tool sets lead optimization and change in all sizes of organizations.</p>
-                
-                </div>
-                <div>
-                    <p>
+           <div className="flexRow margin-top-20px">
+            <div className="width50per">
+                <div className="text-align-center margin-top-20px ">
+                    <p className="text-align-center">
                     Notable technology implementations by our team in reference to these products:
                     </p>
-                    <ul className="acomplishments-ul">
-                        <li className="reveal">
+                    <ul className="acomplishments-ul text-align-center">
+                        <li className=" text-align-center">
                         1st to implement longshore time tracking solution on US West Coast
                         </li>
-                        <li className="reveal">
+                        <li className="text-align-center">
                         1st to implement OCR, EPS technology on US West Coast
                         </li>
-                        <li className="reveal">
+                        <li className="text-align-center">
                         2+ decades of enhancing MTOs Business Intelligence Environments
                         </li>
                     </ul>
                 </div>
-                
-
-                
             </div>
+            <div className="width50per text-align-center borders-20 fLARGE fWEIGHT70 fWHITE bgRED">
+                <p>Cherry America brings 80+ years of combined experience working in marine terminal operations and the shipping industry.</p>
+            </div>
+           </div>
             <div className="ribbon">
                 <strong className="ribbon-content">Leadership Team</strong>
             </div>
             <div className="headshots">
-                <div className="headshot-container-divs">
-                    <div className="brian">
+                <div className="headshot-container-divs ">
+                    <div className="brian round">
                     </div>
-                    <div className="about-text">
+                    <div className="about-text bgLTBLUE fBLACK br-10 text-align-center">
                         <strong>Brian Boyle</strong>
-                       <br></br> Owner, Executive Resource, 30+ years of Maritime and Marine Terminal experience
+                    </div>
+                    <div className="about-text br-10">
+                       Owner, Executive Resource, 30+ years of Maritime and Marine Terminal experience
                     </div>
                 </div>
                 <div className="headshot-container-divs">
-                    <div className="damon">
+                    <div className="damon round">
                     </div>
-                    <div className="about-text">
+                    <div className="about-text br-10 text-align-center bgLTBLUE fBLACK">
                         <strong>Damon Gomes</strong>
-                       <br></br><br></br>COO, 20+ years of Maritime and Marine Terminal experience 
+                      
+
+                    </div>
+                    <div className="about-text br-10">
+                       COO, 20+ years<br></br> of Maritime and<br></br> Marine Terminal experience<br></br>
 
                     </div>
                 </div>
                 <div className="headshot-container-divs">
-                    <div className="norm">
+                    <div className="norm round">
                     </div>
-                    <div className="about-text">
+                    <div className="about-text br-10 text-align-center bgLTBLUE fBLACK">
                         <strong>Norm Kaiser</strong>
-                       <br></br>Operation Resource,<br></br> 25+ years of Maritime and Marine Terminal experience
+                    </div>
+                    <div className="about-text br-10">
+                   Operation Resource,<br></br> 25+ years of Maritime and Marine Terminal experience
                     </div>
                 </div>
                 <div className="headshot-container-divs">
-                    <div className="jim">
+                    <div className="jim round">
                     </div>
-                    <div className="about-text">
+                    <div className="about-text br-10 text-align-center bgLTBLUE fBLACK">
                         <strong>Jim Britton</strong>
-                       <br></br> CFO, 30+ years of Finance in Maritime and Marine Terminal experience
+                    </div>
+                    <div className="about-text br-10">
+                        CFO, 30+ years of Finance in Maritime and Marine Terminal experience
                     </div>
                 </div>
                 
